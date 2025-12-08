fb32e5abff39440676f6e8d427da767c2c151aed
Miyzwan <dimasdwiisma@gmail.com>
2025-12-09 05:30:46 +0700

    update firebase part 6 end

--- app/src/main/java/id/ac/binus/taskflow/signup.java
+++ app/src/main/java/id/ac/binus/taskflow/signup.java
@@ -10,16 +10,25 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class signup extends AppCompatActivity {
 
     private EditText msignuppassword, msignupemail;
     private RelativeLayout msignup;
     private TextView mgotologin;
+
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,12 +41,14 @@ public class signup extends AppCompatActivity {
             return insets;
         });
 
-        getActionBar().hide();
+//        getActionBar().hide();
 
         msignupemail=findViewById(R.id.signupemail);
         msignuppassword=findViewById(R.id.signuppassword);
         msignup=findViewById(R.id.signup);
         mgotologin=findViewById(R.id.gotologin);
+
+        firebaseAuth = FirebaseAuth.getInstance();
 
         mgotologin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -61,9 +72,38 @@ public class signup extends AppCompatActivity {
                 }
                 else{
                     //registered the user to firebase
+                    firebaseAuth.createUserWithEmailAndPassword(mail, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful()){
+                                Toast.makeText(getApplicationContext(), "User Has Been Registered Successfully", Toast.LENGTH_SHORT).show();
+                                sendEmailVerification();
+                            }
+                            else{
+                                Toast.makeText(getApplicationContext(), "Failed To Register", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
                 }
             }
         });
+    }
 
+    private void sendEmailVerification() {
+            FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+            if (firebaseUser != null) {
+                firebaseUser.sendEmailVerification().addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        Toast.makeText(getApplicationContext(), "Verification Email Has Been Sent, Verify and Login again", Toast.LENGTH_SHORT).show();
+                        firebaseAuth.signOut();
+                        finish();
+                        startActivity(new Intent(signup.this, MainActivity.class));
+                    }
+                });
+            }
+            else {
+                Toast.makeText(getApplicationContext(), "Failed To Send Verification Email", Toast.LENGTH_SHORT).show();
+            }
     }
 }
